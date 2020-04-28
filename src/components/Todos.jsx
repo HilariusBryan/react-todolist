31e66cc9f826888481cdba038196275fb12d9422
HilariusBryan <50141190+HilariusBryan@users.noreply.github.com>
2020-04-28 20:05:47 +0700

    redctor todos dan todolist

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,33 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Todo from "./Todo";
-const Todos = () => {
-  const todos = [
-    {
-      text: "Belajar React"
-    },
-    {
-      text: "Belajar props"
-    },
-    {
-      text: "Belajar React Hooks"
-    },
-    {
-      text: "Belajar  State React"
-    },
-    {
-      text: "Belajar React"
-    },
-    {
-      text: "Belajar props"
-    },
-    {
-      text: "Belajar React Hooks"
-    },
-    {
-      text: "Belajar  State React"
-    }
-  ];
 
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map(todo => {
@@ -35,6 +10,14 @@ const Todos = () => {
       })}
     </section>
   );
+};
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string
+    })
+  )
 };
 
 export default Todos;
