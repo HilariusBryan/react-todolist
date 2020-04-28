cb0f55e9e5f5ebf51f98e74f7cdb9166ae511003
HilariusBryan <50141190+HilariusBryan@users.noreply.github.com>
2020-04-28 16:37:28 +0700

    membuat pages dan component to do

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,20 +1,38 @@
 import React from "react";
-
+import Todo from "./Todo";
 const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar props"
+    },
+    {
+      text: "Belajar React Hooks"
+    },
+    {
+      text: "Belajar  State React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar props"
+    },
+    {
+      text: "Belajar React Hooks"
+    },
+    {
+      text: "Belajar  State React"
+    }
+  ];
+
   return (
     <section className="todos">
-      <div className="todo">
-        <span className="todo-text">Belajar React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Belajar React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Belajar React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Belajar React</span>
-      </div>
+      {todos.map(todo => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
